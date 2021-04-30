51a902908376713ed05fd75241ddf52c340f0b15
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-04-30 10:20:39 +0200

    Update the app to read a gdb file

--- app/hydrosheds_app.cc
+++ app/hydrosheds_app.cc
@@ -1,10 +1,35 @@
 #include "hydrosheds/hydrosheds.hh"
 
+#include "ogrsf_frmts.h"
+
 #include<iostream>
 
-int main()
+
+int main(int argc, char** argv)
 {
-  std::cout << "Hello World" << std::endl;
+  if (argc != 2) {
+    std::cerr << "Usage: ./hydrosheds_app <path-to-gdb-directory>" << std::endl;
+    return 1;
+  }
+
+  GDALAllRegister();
+
+  GDALDataset* data;
+  data = (GDALDataset*) GDALOpenEx(argv[1], GDAL_OF_VECTOR, NULL, NULL, NULL );
+
+  if(data == NULL)
+  {
+      std::cerr << "Opening geodatabase failed." << std::endl;
+      return 1;
+  }
+
+  std::cout << "Layer count in GeoDataBase: " << data->GetLayerCount() << std::endl;
+
+  // Extract the first layer
+  auto layer = data->GetLayer(0);
+
+  // Extract first feature
+  auto feature = layer->GetNextFeature();
 
   return 0;
 }
