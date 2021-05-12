fb5497d04e944df4fa74c0fe85335f12bd5ec69b
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2021-05-12 02:29:50 +0200

    Proposal (Incomplete)

--- Proposal/interface.hh
+++ Proposal/interface.hh
@@ -19,11 +19,14 @@ class RiverData
         U operator[](const int& i) const;                                               // returns row i (conversion to vector)
         U get_feature(const std::string& feature_name) const;                           // returns column data of that feature  
         U operator()(const int& i, const int& j) const;                                 // returns (i, j)th entry
-        U get_river_path(const std::string& river_id) const;                            // returns vector path of river given the id
-        U get_associated_rivers(const std::string& river_id) const;                     // returns ids of rivers connecting to the river(id)
+        U get_associated_rivers(const std::string& river_id) const;                     // returns ids of rivers connecting to the river (id)
         double discharge(const std::string& river_id) const ;                           // returns discharge of river provided id
-        std::string main_river() const;                                                 // reuturns main_river ID
-        std::vector <string> get_rivers_catchment(const std::string& river_id) const;
-                                                                                        // reuturns main_river ID
+        std::string main_river() const;                                                 // returns main_river ID
+        std::vector <string> get_rivers_catchment(const std::string& river_id) const;   // returns river IDs in the catchement area of river (id)
+
+        /* data geometry */
+        std::vector <double> get_river_path(const std::string& river_id) const;         // returns vector path of river given the id
+
+                                                                                        
 
 };
