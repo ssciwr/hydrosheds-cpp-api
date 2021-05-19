41fd120f99abb0033f1cf989faf3718f28605c79
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2021-05-20 01:56:31 +0200

    Incomplete Interface

--- app/hydrosheds_app.cc
+++ app/hydrosheds_app.cc
@@ -2,8 +2,68 @@
 
 #include "ogrsf_frmts.h"
 
-#include<iostream>
+#include <iostream>
+#include <vector>
 
+using namespace hydrosheds;
+
+/* -- CLASS HYDROSHEDS DATA SET -- */
+HydroshedsDataSet::HydroshedsDataSet(const char* path)
+{
+    GDALAllRegister();
+    GDALDataset* data;
+    data = (GDALDataset*) GDALOpenEx(path, GDAL_OF_VECTOR, NULL, NULL, NULL);
+    if(data == NULL)
+    {
+        std::cerr << "Opening geodatabase failed." << std::endl;
+    }
+    layer = data->GetLayer(0);
+}
+
+std::array <int, 2> HydroshedsDataSet::shape() const
+{
+    OGRFeature* feature;
+    int count = 0;
+    while((feature = layer->GetNextFeature()) != nullptr)
+    {
+        count++;
+    }
+    // check this count may return nothing as feature maybe pointing to nullptr
+    std::array <int, 2> shape = {count, feature->GetFieldCount()};
+    return shape;
+}
+
+void HydroshedsDataSet::FeatureAttributes() const
+{
+    OGRFeature* feature;
+    feature = layer->GetNextFeature();
+    for(int i = 0; i < feature->GetFieldCount(); i++)
+    {
+        // GetDefnRef() returns an object of the OGRFeatureDefn class. GetFieldDefn() returns an object of the class OGRFeatureDefn
+        std::cout << feature->GetDefnRef()->GetFieldDefn(i)->GetNameRef() << " ";
+    }
+}
+
+RiverSegment HydroshedsDataSet::ConstructSegment(const int& i) const
+{
+    OGRFeature* f = layer->GetFeature(i);
+    RiverSegment s(f);
+    return s;
+}
+
+
+/* -- CLASS RIVER SEGMENT -- */
+RiverSegment::RiverSegment(OGRFeature* f): feature(f) {}
+
+void RiverSegment::test_geometry() const
+{
+    OGRGeometry* G;
+    G = feature->GetGeometryRef();
+    std::cout << "Number of geometrical attributes: " << feature->GetGeomFieldCount() << std::endl;
+    std::cout << G->getGeometryName() << std::endl;
+    std::cout << G->getDimension() << std::endl;
+    OGRGeometry* GLine = G->getLinearGeometry();
+}
 
 int main(int argc, char** argv)
 {
@@ -12,46 +72,33 @@ int main(int argc, char** argv)
     std::cerr << "Usage: ./hydrosheds_app <path-to-gdb-directory>" << std::endl;
     return 1;
     }
+    // Intialise the data set
+    HydroshedsDataSet D(argv[1]);
+    // Initialise a river segment object specify the river by index
+    RiverSegment R = D.ConstructSegment(13);
+    R.test_geometry();
 
-    GDALAllRegister();
-
-    GDALDataset* data;
-    data = (GDALDataset*) GDALOpenEx(argv[1], GDAL_OF_VECTOR, NULL, NULL, NULL );
-
-    if(data == NULL)
-    {
-        std::cerr << "Opening geodatabase failed." << std::endl;
-        return 1;
-    }
-
-    OGRLayer* layer;
-    OGRFeature* feature;
-
-    layer = data->GetLayer(0);
-
-
-  // Extract layers
-    for(auto&& oField: *(layer->GetNextFeature()))
-    {
-        switch(oField.GetType())
-        {
-            case OFTInteger:
-                printf( "%d,", oField.GetInteger());
-                break;
-            case OFTInteger64:
-                printf( CPL_FRMT_GIB ",", oField.GetInteger64());
-                break;
-            case OFTReal:
-                printf( "%.3f,", oField.GetDouble());
-                break;
-            case OFTString:
-                printf( "%s,", oField.GetString());
-                break;
-            default:
-                printf( "%s,", oField.GetAsString());
-                break;
-        }
-    }
-
+    // Extract layers
+    // for(auto&& oField: *(layer->GetNextFeature()))
+    // {
+    //     switch(oField.GetType())
+    //     {
+    //         case OFTInteger:
+    //             printf( "%d,", oField.GetInteger());
+    //             break;
+    //         case OFTInteger64:
+    //             printf( CPL_FRMT_GIB ",", oField.GetInteger64());
+    //             break;
+    //         case OFTReal:
+    //             printf( "%.3f,", oField.GetDouble());
+    //             break;
+    //         case OFTString:
+    //             printf( "%s,", oField.GetString());
+    //             break;
+    //         default:
+    //             printf( "%s,", oField.GetAsString());
+    //             break;
+    //     }
+    // }
     return 0;
 }
--- include/hydrosheds/hydrosheds.hh
+++ include/hydrosheds/hydrosheds.hh
@@ -1,5 +1,55 @@
 #pragma once
 
-namespace hydrosheds {
+#include "ogrsf_frmts.h"
 
-} // namespace hydrosheds
+#include <array>
+#include <string>
+
+namespace hydrosheds 
+{
+
+  using Coordinate = std::array <double, 2>;
+  using HydroshedsID = int;
+
+  // Forward declaration
+  class RiverSegment;
+
+  class HydroshedsDataSet
+  {
+    public:
+		// GDALOpenex accepts a const char *
+		HydroshedsDataSet() = default;
+		HydroshedsDataSet(const char* s);
+		std::array <int, 2> shape() const;
+		void FeatureAttributes() const;
+		RiverSegment ConstructSegment(const int& i) const;
+
+    private:
+		OGRLayer* layer;
+  };
+
+
+  class RiverSegment
+  {
+    public:
+		void test_geometry() const;
+		double getLength() const;
+		double getGeologicalLength() const;
+		double getDischarge() const;
+
+		Coordinate getStartingPoint() const;
+		Coordinate getEndPoint() const;
+
+		bool hasDownstreamSegment() const;
+		RiverSegment getDownstreamSegment() const;
+		// What about upstream segments - is it possible?
+
+    private:
+		RiverSegment(OGRFeature* feature);
+		OGRFeature* feature;
+		unsigned int segment;
+		
+	friend class HydroshedsDataSet;
+  };
+
+} 
