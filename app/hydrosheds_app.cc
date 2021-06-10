f600a00003a619e5c873a1f6f7bd04143d8452ac
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2021-06-10 21:03:44 +0200

    interface almost done

--- app/hydrosheds_app.cc
+++ app/hydrosheds_app.cc
@@ -3,7 +3,7 @@
 #include "ogrsf_frmts.h"
 
 #include <iostream>
-#include <vector>
+#include <cmath>
 #include <iomanip>
 
 using namespace hydrosheds;
@@ -53,6 +53,16 @@ void HydroshedsDataSet::FeatureAttributes() const
     }
 }
 
+OGRFeature* HydroshedsDataSet::search_feature(unsigned int ID) const
+{
+    int count = 0;
+    while(layer->GetNextFeature()->GetFieldAsInteger("HYRIV_ID") != ID)
+    {
+        count++;
+    }
+    return layer->GetFeature(count);
+}
+
 RiverSegment HydroshedsDataSet::ConstructSegment(const int& i) const
 {
     OGRFeature* f = layer->GetFeature(i);
@@ -63,81 +73,121 @@ RiverSegment HydroshedsDataSet::ConstructSegment(const int& i) const
 
 /* -- CLASS RIVER SEGMENT -- */
 RiverSegment::RiverSegment(OGRFeature* f)
-    : feature(f) 
-    {
-        geometry = feature->GetGeometryRef();
-    }
-
-void RiverSegment::test_geometry() const
+    : feature(f)
 {
-    // std::cout << "Number of geometrical attributes: " 
-    //           << feature->GetGeomFieldCount() << std::endl;
-    OGRMultiLineString* GLine = geometry->toMultiLineString();
-    std::cout << std::setprecision(13);
+    // set geometry object
+    geometry = feature->GetGeometryRef();
+    GLine = geometry->toMultiLineString();
     for(auto multi_line_string: *(GLine))
     {
         for(auto point: *(multi_line_string))
         {
-            // point is an OGRPoint
-            std::cout << point.getX() << ", " <<  point.getY() << std::endl;
+            // set segment start and end points
+            // point is an OGRPoint 
+            Coordinate p = {point.getX(), point.getY()};
+            segment_points.push_back(p);
         }
+    }
+}
+
+void RiverSegment::test_geometry() const
+{
+    std::cout << std::setprecision(13);
+    for(auto point: segment_points)
+    {
+        std::cout << point[0] << ", " <<  point[1] << std::endl;
     }
     std::cout << std::setprecision(2);
 }
 
 int RiverSegment::get_number_of_subsegments() const
 {
-    OGRMultiLineString* GLine = geometry->toMultiLineString();
-    int count = 0;
-    for(auto multi_line_string: *(GLine))
+    return segment_points.size() % 2 == 0 ? segment_points.size() / 2 : segment_points.size() / 2 + 1;
+}
+
+double RiverSegment::getLength(int sub_segment) const
+{
+    Coordinate p1 = this->getStartingPoint(sub_segment);
+    Coordinate p2 = this->getEndPoint(sub_segment); 
+    return std::sqrt(std::pow(71.5 * (p1[0] - p2[0]), 2) + std::pow(111.3 * (p1[1] - p2[1]), 2));
+}
+
+double RiverSegment::getTotalLength() const
+{
+    double total_length = 0.0;
+    for(int i = 0; i < this->get_number_of_subsegments(); i++)
     {
-        for(auto point: *(multi_line_string))
-        {
-            count++;
-        }
+        Coordinate p1 = this->getStartingPoint(i);
+        Coordinate p2 = this->getEndPoint(i); 
+        total_length += std::sqrt(std::pow(71.5 * (p1[0] - p2[0]), 2) + std::pow(111.3 * (p1[1] - p2[1]), 2)); 
     }
-    return count;
+    return total_length;
+}
+
+double RiverSegment::getGeologicalLength() const
+{
+    return feature->GetFieldAsDouble("LENGTH_KM");
+}
+
+double RiverSegment::getDischarge() const
+{
+    return feature->GetFieldAsDouble("DIS_A V_CMS");
 }
 
 Coordinate RiverSegment::getStartingPoint(int sub_segment) const
 {
-    OGRMultiLineString* GLine = geometry->toMultiLineString();
     int count = 0;
-    Coordinate p;
-    for(auto multi_line_string: *(GLine))
+    for(int i = 0; i < segment_points.size(); i += 2)
     {
-        for(auto point: *(multi_line_string))
+        if (count == sub_segment)
         {
-            if(count == sub_segment)
-            {
-                p = {point.getX(), point.getY()};
-            }
-            count++;
+            return segment_points[i];
         }
-    }
-    return p;
-}
+        count += 1;
 
+    }
+}
 
 Coordinate RiverSegment::getEndPoint(int sub_segment) const
 {
-    OGRMultiLineString* GLine = geometry->toMultiLineString();
     int count = 0;
-    Coordinate p;
-    for(auto multi_line_string: *(GLine))
+    for(int i = 1; i < segment_points.size(); i += 2)
     {
-        for(auto point: *(multi_line_string))
+        if (count == sub_segment)
         {
-            if(count == sub_segment + 1)
-            {
-                p = {point.getX(), point.getY()};
-            }
-            count++;
+            return segment_points[i];
         }
+        count += 1;
     }
-    return p; 
 }
 
+bool RiverSegment::hasDownstreamSegment() const
+{
+    if(feature->GetFieldAsInteger("NEXT_DOWN") == 0 || feature->GetFieldAsInteger("ENDORHEIC") == 1)
+    {
+        return false;
+    }
+    return true;
+}
+
+void RiverSegment::get_ID() const
+{
+    std::cout << feature->GetFID() << std::endl;
+}
+
+
+RiverSegment RiverSegment::getDownstreamSegment() const
+{
+    if(this->hasDownstreamSegment() == false)
+    {
+        std::cerr << "No downstream segments for current segment." << std::endl;
+    }
+
+    int current_ID = feature->GetFID();
+
+    OGRFeature* f = this->search_feature(current_ID);
+
+}
 
 
 int main(int argc, char** argv)
@@ -152,15 +202,21 @@ int main(int argc, char** argv)
     
     // Initialise a river segment object specified by the river by index
     // See feature 27005 using ogrinfo -al <path_to_data> 
-    RiverSegment R = D.ConstructSegment(27005);
+    RiverSegment R = D.ConstructSegment(3127);
     R.test_geometry();
 
     // See number of subsegments in the chosen feature
-    std::cout << R.get_number_of_subsegments() << std::endl;
+    std::cout << "Number of segments: " << R.get_number_of_subsegments() << std::endl;
 
 
     // Start and end points test
-    print_coordinate(R.getStartingPoint(0));
-    print_coordinate(R.getEndPoint(0));
+    print_coordinate(R.getStartingPoint(5));
+    print_coordinate(R.getEndPoint(5));
+    std::cout << std::setprecision(4);
+    std::cout << "Length of 6th subsegment of current river segment: " << R.getLength(5) << " Km" << std::endl;
+    std::cout << "Total length of current river segment: " << R.getTotalLength() << "Km" << std::endl;
+    std::cout << "Geological length of current river segment: " << R.getGeologicalLength() << " Km" << std::endl;
+    std::cout << "Discharge of current river segment: " << R.getDischarge() << " m3/s" << std::endl;
+    R.get_ID();
     return 0;
 }
--- include/hydrosheds/hydrosheds.hh
+++ include/hydrosheds/hydrosheds.hh
@@ -4,60 +4,61 @@
 
 #include <array>
 #include <string>
+#include <vector>
 
 namespace hydrosheds 
 {
 
-  using Coordinate = std::array <double, 2>;
-  using HydroshedsID = int;
+	using Coordinate = std::array <double, 2>;
+	using HydroshedsID = int;
 
-  // Forward declaration
-  class RiverSegment;
+	// Forward declaration
+	class RiverSegment;
 
-  class HydroshedsDataSet
-  {
-    public:
+	class HydroshedsDataSet
+	{
+	public:
 		// GDALOpenex accepts a const char *
 		HydroshedsDataSet() = default;
 		HydroshedsDataSet(const std::string&);
 		std::array <int, 2> shape() const;
 		void FeatureAttributes() const;
+		OGRFeature* search_feature(unsigned int) const;
 		RiverSegment ConstructSegment(const int& i) const;
 
-    private:
+	private:
 		OGRLayer* layer;
-  };
+	};
 
 
-  class RiverSegment
-  {
-    public:
+	class RiverSegment
+	{
+	public:
 		void test_geometry() const;
 		int get_number_of_subsegments() const;
-		double getLength() const;
+		double getLength(int) const;
 		double getTotalLength() const;
 		double getGeologicalLength() const;
 		double getDischarge() const;
 
 		Coordinate getStartingPoint(int) const;
 		Coordinate getEndPoint(int) const;
+		void get_ID() const;
 
 		bool hasDownstreamSegment() const;
 		RiverSegment getDownstreamSegment() const;
 		// What about upstream segments - is it possible?
 
-    private:
+	private:
 		RiverSegment(OGRFeature* feature);
 		OGRFeature* feature;
 		OGRGeometry* geometry;
-		unsigned int segment;
+		OGRMultiLineString* GLine;
+		std::vector <Coordinate> segment_points;  
+		unsigned long int segment;
 		
-	friend class HydroshedsDataSet;
-  };
-
-  /* -- SUBSEGMENT CLASS -- */
-  // Holds point data and stuff? 
+		friend class HydroshedsDataSet;
+	};
 
 } 
 
-// Should segment be one segment or the number of segments? 
