62ab815ed5f0013b97e3f3d613d15ebdea37b407
Mert Biyikli <biyikli_ibrahim@hotmail.com>
2021-06-21 17:38:19 +0200

    Added the documentation comments to FullDataSetRiverSegmentIterator

--- app/hydrosheds_app.cc
+++ app/hydrosheds_app.cc
@@ -6,56 +6,32 @@
 
 int main(int argc, char** argv)
 {
-  if (argc != 2) {
-    std::cerr << "Usage: ./hydrosheds_app <path-to-gdb-directory>" << std::endl;
-    return 1;
-  }
+    if (argc != 2) {
+        std::cerr << "Usage: ./hydrosheds_app <path-to-gdb-directory>" << std::endl;
+        return 1;
+    }
 
-  // Instantiate the data set
-  hydrosheds::HydroshedsDataSet dataset(argv[1]);
+    // Instantiate the data set
+    hydrosheds::HydroshedsDataSet dataset(argv[1]);
 
-  // ./hydrosheds_app /Users/halilibrahim/Desktop/HydroRIVERS_v10_eu/HydroRIVERS_v10_eu.gdb
+    // ./hydrosheds_app /Users/halilibrahim/Desktop/HydroRIVERS_v10_eu/HydroRIVERS_v10_eu.gdb
     std::cout << "Checking some things: " << std::endl;
-
-
-
-    for(auto it = dataset.begin(); 1; ++it)
-    {
+/**
+    /// running it with prefix
+    for(auto it = dataset.begin(); it!=dataset.end(); ++it)
         std::cout << it->getLength() << std::endl;
-    }
-
-    std::cout << "Before the loop " << std::endl;
-    auto start = dataset.begin();
-    std::cout << start->getLength() << std::endl;
-
-    /// using the free function
-/*
-    while(1)
-    {
-        auto next = increment(start);
-        std::cout << start->getLength() << std::endl;
-        start = next;
-    }*/
-/*
-    while(1) {
-        auto next = start.layer->GetNextFeature();
-        hydrosheds::FullDatasetRiverSegmentIterator it1(next, start.layer);
-        if(dataset.end() == it1)
-        {
-            break;
-        }
-        std::cout << it1->getLength() << std::endl;
-    }*/
-
-/*
-    while(1)
-    {
-        start++;
-        std::cout << start->getLength() << std::endl;
-    }
-    std::cout << "After loop" << std::endl;
+*/
+/**
+    /// running it with postfix
+    for(auto it = dataset.begin(); it!=dataset.end(); it++)
+        std::cout << it->getLength() << std::endl;
 */
 
 
-  return 0;
+
+    /// ranged based loop
+    for(auto it : dataset)
+        std::cout << it.getLength() << std::endl;
+
+    return 0;
 }
--- include/hydrosheds/hydrosheds.hh
+++ include/hydrosheds/hydrosheds.hh
@@ -7,89 +7,89 @@
 
 namespace hydrosheds {
 
-  using Coordinate = std::array<double, 2>;
-  using HydroshedsID = int;
+    using Coordinate = std::array<double, 2>;
+    using HydroshedsID = int;
 
-  // Forward declarations
-  class RiverSegment;
-  class FullDatasetRiverSegmentIterator;
+    // Forward declarations
+    class RiverSegment;
+    class FullDatasetRiverSegmentIterator;
 
-  class HydroshedsDataSet
-  {
+    class HydroshedsDataSet
+    {
     public:
-    HydroshedsDataSet(const std::string&);
+        HydroshedsDataSet(const std::string&);
 
-    // Iterator access
-    FullDatasetRiverSegmentIterator begin() const; /// it compiles but
-    FullDatasetRiverSegmentIterator end() const;  /// stops after one iteration
-    // Not intended to stay - just get some segment
-    RiverSegment getSegment() const;
+        // Iterator access
+        FullDatasetRiverSegmentIterator begin() const; /// it compiles but
+        FullDatasetRiverSegmentIterator end() const;  /// stops after one iteration
+        // Not intended to stay - just get some segment
+        RiverSegment getSegment() const;
 
     private:
-    OGRLayer* layer;
-    long count;
-  };
+        OGRLayer* layer;
+        long count;
+    };
 
 
-  class RiverSegment
-  {
+    class RiverSegment
+    {
     public:
-    double getLength() const;
-    double getGeologicalLength() const;
-    double getDischarge() const;
+        double getLength() const;
+        double getGeologicalLength() const;
+        double getDischarge() const;
 
-    Coordinate getStartingPoint() const;
-    Coordinate getEndPoint() const;
+        Coordinate getStartingPoint() const;
+        Coordinate getEndPoint() const;
 
-    OGRFeature* getFeature(){return feature;};
-    OGRLayer* getLayer(){return layer;};
+        OGRFeature* getFeature(){return feature;};
+        OGRLayer* getLayer(){return layer;};
 
-    bool hasDownstreamSegment() const;
-    RiverSegment getDownstreamSegment() const;
-    // What about upstream segments - is it possible?
-
-
-    private:
-    friend class HydroshedsDataSet;
-    friend class FullDatasetRiverSegmentIterator;
-
-    RiverSegment(OGRLayer*, OGRFeature*);
-    OGRLayer* layer;
-    OGRFeature* feature;
-  };
+        bool hasDownstreamSegment() const;
+        RiverSegment getDownstreamSegment() const;
+        // What about upstream segments - is it possible?
 
 
+    //private:
+        friend class HydroshedsDataSet;
+        friend class FullDatasetRiverSegmentIterator;
 
-  class FullDatasetRiverSegmentIterator
-  {
-  public:
-    OGRFeature *feature;
-    OGRLayer *layer;
-    RiverSegment segment;
-
-    FullDatasetRiverSegmentIterator(OGRFeature* ogrFeature, OGRLayer* ogrLayer);
-
-
-      /// to get a pointer and reference access
-      RiverSegment* operator->();
-      RiverSegment& operator*();
-
-      /// incrementing via prefix
-      FullDatasetRiverSegmentIterator operator++();
-
-      /// incrementing via postfix
-      FullDatasetRiverSegmentIterator operator++(int);
-
-      /// equality operations
-      bool operator==(const FullDatasetRiverSegmentIterator& a);
-      bool operator!=(const FullDatasetRiverSegmentIterator& a);
+        RiverSegment(OGRLayer*, OGRFeature*);
+        OGRLayer* layer;
+        OGRFeature* feature;
+    };
 
 
-      /// Not intendent to say
-      OGRFeature* getFeature() const {return feature;};
-      OGRLayer* getLayer() const {return layer;};
 
-  };
+    class FullDatasetRiverSegmentIterator
+    {
+    public:
+        OGRFeature *feature;
+        OGRLayer *layer;
+        RiverSegment segment;
+
+        FullDatasetRiverSegmentIterator(OGRFeature* ogrFeature, OGRLayer* ogrLayer);
+
+
+        /// to get a pointer and reference access
+        RiverSegment* operator->();
+        RiverSegment& operator*();
+
+        /// incrementing via prefix
+        FullDatasetRiverSegmentIterator operator++();
+
+        /// incrementing via postfix
+        FullDatasetRiverSegmentIterator operator++(int);
+
+        /// equality operations
+        bool operator==(const FullDatasetRiverSegmentIterator& a);
+        bool operator!=(const FullDatasetRiverSegmentIterator& a);
+
+
+        /// Not intendent to say
+        OGRFeature* getFeature() const {return feature;};
+        OGRLayer* getLayer() const {return layer;};
+
+    };
 
     FullDatasetRiverSegmentIterator increment(hydrosheds::FullDatasetRiverSegmentIterator it);
 
