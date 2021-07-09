9ba0096006fa4af1cb0c9e69b955c2993d4ca6c9
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2021-07-09 23:36:35 +0200

    working now

--- doc/conf.py
+++ doc/conf.py
@@ -72,5 +72,4 @@ if read_the_docs_build:
     subprocess.check_call("cmake -DBUILD_DOCS=ON -DBUILD_TESTING=OFF  ../..".split(), cwd=builddir)
     subprocess.check_call("cmake --build . --target doxygen".split(), cwd=builddir)
     breathe_projects["hydrosheds-cpp-api"] = os.path.join(builddir, "doc", "xml")
-else:
-    breathe_projects["hydrosheds-cpp-api"] = os.path.join(os.environ["HYDROSHEDS_BUILD_DIR"], "doc", "xml")
+
